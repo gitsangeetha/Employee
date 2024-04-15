00989bc1099c9228eab9b0f835305b18c5363c11
sangeethagitacc <sangeetha04raghul@gmail.com>
2024-04-15 12:39:48 +0530

    functional

---
+++ src/test/java/Employee.java
@@ -0,0 +1,20 @@
+
+public class Employee {
+	private int empid;
+	private String empname;
+
+	public void Employe() {
+		//super();
+		this.empid=empid;
+		this.empname=empname;
+	}
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpname() {
+	return empname;
+}
+}
